e30d912ecbcec3019a9c2799abb8f99ed79408e1
ethan528 <ethan528@naver.com>
2022-11-26 21:10:54 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 2;
-
-        for (int cnt = 1; cnt <= 10; cnt++) {
-            System.out.println(a);
-            a = a * b;
-        }
+        System.out.println(1+10);
     }
 }
