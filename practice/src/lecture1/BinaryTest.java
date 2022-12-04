d3370b4c27f2ec7f7661e6d5aa17b5f66ea92cbd
ethan528 <ethan528@naver.com>
2022-12-04 22:41:24 +0900

    FC chapter 1

---
+++ practice/src/lecture1/BinaryTest.java
@@ -0,0 +1,15 @@
+package lecture1;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        int num = 10; // 10진수
+        int bNum = 0b1010; // 2진수, 식별자 0b
+        int oNum = 012; // 8진수, 식별자 0
+        int xNum = 0xA; // 16진수, 식별자 0x
+
+        System.out.println(num);
+        System.out.println(bNum);
+        System.out.println(oNum);
+        System.out.println(xNum);
+    }
+}
---
+++ practice/src/lecture1/VariableTest.java
@@ -0,0 +1,12 @@
+package lecture1;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        byte bnum = 127;
+        System.out.println(bnum);
+
+        // int num = 12345679800;
+        long lNum = 12345678900L;
+        System.out.println(lNum);
+    }
+}
